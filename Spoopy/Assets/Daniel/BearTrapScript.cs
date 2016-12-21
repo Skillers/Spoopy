9be2f3e7d31a3607a1465fda21c256476c66dfb9
Daniel <dsrooth@gmail.com>
2016-12-21 16:54:09 +0100

    Bear Trap Skill @key 2

---
+++ Spoopy/Assets/Daniel/BearTrapScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BearTrapScript : MonoBehaviour {
+    private float effectTimer;
+    public float effectDuration = 5;
+
+    GameObject target;
+    private bool triggered;
+	// Use this for initialization
+	void Start () {
+        effectTimer = 0;
+        triggered = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (triggered)
+        {
+            effectTimer += Time.deltaTime;
+            target.GetComponent<NavMeshAgent>().Stop();
+            if (effectTimer >= effectDuration)
+            {
+                target.GetComponent<NavMeshAgent>().Resume();
+                Destroy(gameObject);
+                effectTimer = 0;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Resident")
+        {
+            target = other.gameObject;
+            triggered = true;
+        }
+
+    }
+}
