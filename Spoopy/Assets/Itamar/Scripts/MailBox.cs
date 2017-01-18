596e38c40e1f19588b06a2275816a65c2b817b5c
Itamar Efrat <Itamar.Efrat@hva.nl>
2017-01-18 17:57:03 +0100

    Lights outside, spots, tree and tomb. Imports of new lights done.

---
+++ Spoopy/Assets/Itamar/Scripts/MailBox.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MailBox : MonoBehaviour {
+
+    public bool Activated;
+
+    public void Activate()
+    {
+        if(!Activated)
+        {
+            this.transform.Rotate(0f, 0f, 90f);
+            Activated = true;
+        }
+        else if(Activated)
+        {
+            this.transform.Rotate(0f, 0f, -90f);
+            Activated = false;
+        }
+        //code to bring le residents
+    }
+}
